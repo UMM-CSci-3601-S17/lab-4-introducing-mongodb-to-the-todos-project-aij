72f53c08c8ed6498028ad1f8424e0c6c8414f318
Sphat001 <sphat001@morris.umn.edu>
2017-02-23 13:47:55 -0600

    Create Todo.java and TodoController.java.

--- server/src/main/java/umm3601/todo/Todo.java
+++ server/src/main/java/umm3601/todo/Todo.java
@@ -1,7 +1,5 @@
 package umm3601.todo;
 
-/**
- * Created by sphat001 on 2/23/17.
- */
+
 public class Todo {
 }
--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,7 +1,4 @@
 package umm3601.todo;
 
-/**
- * Created by sphat001 on 2/23/17.
- */
 public class TodoController {
 }
