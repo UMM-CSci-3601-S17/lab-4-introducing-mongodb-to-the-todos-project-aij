29060c7bbd1285a693f3c2ca093fa052ff266539
Ai Sano <sanox013@morris.umn.edu>
2017-03-02 22:22:49 -0600

    Add FilterBy functions
    * by owner
    * by status
    * by body

--- client/src/app/todo/todo-list.service.ts
+++ client/src/app/todo/todo-list.service.ts
@@ -8,8 +8,20 @@ export class TodoListService {
     private todoUrl: string = API_URL + "todos";
     constructor(private http:Http) { }
 
-    getTodos(): Observable<Todo[]> {
+    getAllTodos(): Observable<Todo[]> {
         return this.http.request(this.todoUrl).map(res => res.json());
+    }
+
+    filterTodosByOwner(owner: string): Observable<Todo[]> {
+        return this.http.request(this.todoUrl + "?owner=" + owner).map(res => res.json());
+    }
+
+    filterTodosByStatus(status: string): Observable<Todo[]> {
+        return this.http.request(this.todoUrl + "?status=" + status).map(res => res.json());
+    }
+
+    filterTodosByBody(body: string): Observable<Todo[]> {
+        return this.http.request(this.todoUrl + "?body=" + body).map(res => res.json());
     }
 
     getTodoById(id: string): Observable<Todo> {
