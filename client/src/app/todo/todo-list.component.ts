973b35f01f36a933d15d35059249b0c25bb76c03
Ai Sano <sanox013@morris.umn.edu>
2017-03-03 21:29:20 -0600

    Add two functions
    * getFilterVals() to concatenate filtering values
    * clickToFilter() to call filterTodos(filterVals) in todo-list.service.ts

--- client/src/app/todo/todo-list.component.ts
+++ client/src/app/todo/todo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TodoListService } from "./todo-list.service";
 import { Todo } from "./todo";
 import { FilterBy } from "./filter.pipe";
+import {filter} from "rxjs/operator/filter";
 
 @Component({
     selector: 'todo-list-component',
@@ -9,18 +10,52 @@ import { FilterBy } from "./filter.pipe";
     providers: [ FilterBy ]
 })
 
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
     public todos: Todo[];
-    public searchOwner: string;
-    public searchStatus: string;
-    public searchBody: string;
 
     constructor(private todoListService: TodoListService) {
         // this.users = this.userListService.getUsers();
     }
 
-    enterFilterVal(): void {
-        this.todoListService.filterTodosByField(this.searchOwner, this.searchStatus, this.searchBody).subscribe(
+    getFilterVals(): string {
+        var filterVals = "?";
+
+        if ((<HTMLInputElement>document.getElementById("owner")).checked) {
+            filterVals +=
+                "&owner=" + (<HTMLInputElement>document.getElementById("ownerVal")).value;
+        }
+
+        if ((<HTMLInputElement>document.getElementById("body")).checked) {
+            filterVals +=
+                "&body=" + (<HTMLInputElement>document.getElementById("bodyVal")).value;
+        }
+
+        if ((<HTMLInputElement>document.getElementById("category")).checked) {
+            filterVals +=
+                "&category=" + (<HTMLInputElement>document.getElementById("categoryVal")).value;
+        }
+
+        if ((<HTMLInputElement>document.getElementById("status")).checked) {
+            filterVals +=
+                "&status=" + (<HTMLInputElement>document.getElementById("statusVal")).value;
+        }
+
+        return filterVals;
+    }
+
+    clickToFilter(): void {
+        var filterVals = this.getFilterVals();
+
+        this.todoListService.filterTodos(filterVals).subscribe(
+            todos => this.todos = todos,
+            err => {
+                console.log(err);
+            }
+        );
+    }
+
+    ngOnInit(): void {
+        this.todoListService.getTodos().subscribe(
             todos => this.todos = todos,
             err => {
                 console.log(err);
